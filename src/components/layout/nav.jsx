dd52b9692f1fc28d425e50783676ae01086ae0db
adam <adam.kassir@gmail.com>
2025-02-19 23:39:47 +0100

    mis a jour du cv download

--- src/components/layout/nav.jsx
+++ src/components/layout/nav.jsx
@@ -9,7 +9,7 @@ const Nav = (props) => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
                         <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">CV</Link></li>
-                        <li className="nav-item"><a href="/cvs/Adam_Kassir_cv_2022_05.pdf" className="nav-link" target = "_blank">Télécharger cv pdf</a></li>
+                        <li className="nav-item"><a href="/cvs/Adam_Kassir_cv_02_2025.pdf" className="nav-link" target = "_blank">Télécharger cv pdf</a></li>
                         <li className="nav-item"><a className="nav-link active" aria-current="page" href="https://www.linkedin.com/in/adam-kassir/" target="_blank"><i className="fa fa-linkedin"></i> Linkedin</a></li>
                         <li className="nav-item"><a className="nav-link active" aria-current="page" href="https://github.com/spike59" target="_blank"><i className="fa fa-github"></i> Github</a></li>
                         <li className="nav-item"><a className="nav-link active" aria-current="page" href="https://www.codingame.com/profile/572fdbbe3b0d91323fae312e8d31ebed8572483" target="_blank">Coding game</a></li>
